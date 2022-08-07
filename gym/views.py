a7da79e338939f1f420dd33a97670fedf9a31db0
jjw18th <darshu18th@gmail.com>
2022-08-07 10:05:29 +0530

    first commit

--- gym/views.py
+++ gym/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def landing(request):
-    res=render(request,'gym/landing2.html')
+    res=render(request,'gym/index.html')
     return res;
 def about(request):
     res=render(request,'gym/about2.html')
